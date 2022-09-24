51811c458fca616010dbc4d37dd98fcfcab1c509
Abdelfatah Ashour <abdelfatahashour4@gmail.com>
2022-09-24 15:46:50 +0200

    add relative image url

--- client_side/src/components/OneArticle/index.tsx
+++ client_side/src/components/OneArticle/index.tsx
@@ -8,37 +8,20 @@ import { ArticleShape } from "../../utilities/interfaces";
 import { ToggleWishlist } from "../../utilities/useAddWishlist";
 import "./oneArticle.css";
 
-export default function Index({
-  article,
-  currentWidth,
-}: {
-  article: any;
-  currentWidth?: any;
-}) {
-  const { wishlist } = useSelector<
-    DefaultRootState,
-    { wishlist: ArticleShape[] }
-  >((state: RootStateOrAny) => state);
+export default function Index({ article, currentWidth }: { article: any; currentWidth?: any }) {
+  const { wishlist } = useSelector<DefaultRootState, { wishlist: ArticleShape[] }>((state: RootStateOrAny) => state);
 
   return (
     <div className={"one-article p-0 " + currentWidth} key={article._id}>
-      <button
-        className="btn-save"
-        aria-label="saved to favorite"
-        onClick={() => ToggleWishlist(article)}
-      >
-        {wishlist.find((item) => item._id === article._id) ? (
-          <AiFillHeart />
-        ) : (
-          <AiOutlineHeart />
-        )}
+      <button className="btn-save" aria-label="saved to favorite" onClick={() => ToggleWishlist(article)}>
+        {wishlist.find(item => item._id === article._id) ? <AiFillHeart /> : <AiOutlineHeart />}
       </button>
       <div className="wrapper-image">
         <img
           src={
-            process.env.REACT_APP_API_IMAGE +
-            "/v1/image/" +
-            article.imageArticle
+            process.env.NODE_ENV === "production"
+              ? `${process.env.REACT_APP_API_IMAGE}/v1/image/${article.imageArticle}`
+              : `/v1/image/${article.imageArticle}`
           }
           alt={article.title}
           loading="lazy"
@@ -47,16 +30,10 @@ export default function Index({
       </div>
       <div className="d-flex article-details">
         <div className="article-img-user">
-          <img
-            src={user}
-            alt={article.author.firstName + "  " + article.author.lastName}
-            loading="lazy"
-          />
+          <img src={user} alt={article.author.firstName + "  " + article.author.lastName} loading="lazy" />
         </div>
         <div className="details">
-          <span className="text-capitalize">
-            By: {article.author.firstName + "  " + article.author.lastName}
-          </span>
+          <span className="text-capitalize">By: {article.author.firstName + "  " + article.author.lastName}</span>
           <span className="mx-2">⌚ {moment(article.createdAt).fromNow()}</span>
           <h6 className="title my-2">{article.title.slice(0, 55)}</h6>
           <div className="details d-flex justify-content-between align-items-baseline w-100">
@@ -71,11 +48,7 @@ export default function Index({
             </div>
 
             <div className="save-article d-flex justify-content-evenly align-items-center">
-              <Link
-                to={`/article/${article._id}`}
-                aria-label="read full article"
-                rel="next"
-              >
+              <Link to={`/article/${article._id}`} aria-label="read full article" rel="next">
                 <BiLinkAlt /> full article
               </Link>
             </div>
